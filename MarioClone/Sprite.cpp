559bcabbb401f9f6660d41d97daa67f7ac036914
Emma Stitt <emmcooke@optusnet.com.au>
2022-05-04 17:01:46 +1000

    Begun work on sprite class

---
+++ MarioClone/Sprite.cpp
@@ -0,0 +1,6 @@
+#include "Sprite.h"
+
+Sprite::Sprite() :backgroundColour(std::vector<double>{ 0.75, 0.3, 0.75, 1 })
+{
+    
+}
---
+++ MarioClone/Sprite.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+class Sprite
+{
+public:
+    std::vector<double> backgroundColour;
+    Sprite();
+};
+
