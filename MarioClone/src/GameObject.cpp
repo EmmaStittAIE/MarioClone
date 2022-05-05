98ff54d50c6337ec292dd20e232568816afd1a25
Emma Stitt <emmcooke@optusnet.com.au>
2022-05-05 11:13:13 +1000

    Improved formatting

--- MarioClone/src/GameObject.cpp
+++ MarioClone/src/GameObject.cpp
@@ -1,6 +1,7 @@
 #include "GameObject.h"
 
-GameObject::GameObject() :backgroundColour(std::vector<double>{ 0.75, 0.3, 0.75, 1 })
+GameObject::GameObject()
+    :backgroundColour(std::vector<double>{ 0, 0, 0, 1 })
 {
     
 }
